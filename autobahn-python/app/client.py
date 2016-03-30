80911535f677da10ee261827c222488f31829dd7
Tobias Oberstein <tobias.oberstein@tavendo.de>
2016-03-30 15:17:03 +0200

    polish

--- autobahn-python/app/client.py
+++ autobahn-python/app/client.py
@@ -8,6 +8,9 @@ from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
 
 
 class ClientSession(ApplicationSession):
+    """
+    Our WAMP session class .. place your app code here!
+    """
 
     def onConnect(self):
         self.log.info("Client connected")
@@ -19,6 +22,9 @@ class ClientSession(ApplicationSession):
 
     def onJoin(self, details):
         self.log.info("Client session joined {details}", details=details)
+
+        # your main app code goes here! eg register procedures, subscribe to topics, etc
+
         self.leave()
 
     def onLeave(self, details):
@@ -35,21 +41,29 @@ class ClientSession(ApplicationSession):
 
 if __name__ == '__main__':
 
+    # Crossbar.io connection configuration
+    url = os.environ.get('CBURL', u'ws://localhost:8080/ws')
+    realm = os.environ.get('CBREALM', u'realm1')
+
+    # parse command line parameters
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--debug', action='store_true', help='Enable debug output.')
-    parser.add_argument('--url', dest='url', type=six.text_type, default=u'ws://localhost:8080/ws', help='The router URL (default: "ws://localhost:8080/ws").')
-    parser.add_argument('--realm', dest='realm', type=six.text_type, default=u'realm1', help='The realm to join (default: "realm1").')
+    parser.add_argument('--url', dest='url', type=six.text_type, default=url, help='The router URL (default: "ws://localhost:8080/ws").')
+    parser.add_argument('--realm', dest='realm', type=six.text_type, default=realm, help='The realm to join (default: "realm1").')
 
     args = parser.parse_args()
 
+    # start logging
     if args.debug:
         txaio.start_logging(level='debug')
     else:
         txaio.start_logging(level='info')
 
+    # any extra info we want to forward to our ClientSession (in self.config.extra)
     extra = {
         u'foobar': u'A custom value'
     }
 
+    # now actually run a WAMP client using our session class ClientSession
     runner = ApplicationRunner(url=args.url, realm=args.realm, extra=extra)
     runner.run(ClientSession)
