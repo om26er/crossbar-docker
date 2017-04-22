7a4b557e53811f93d7d012060c2690ef3c0d895e
Tobias Oberstein <tobias.oberstein@crossbario.com>
2017-04-22 20:35:50 +0200

    more images

--- images.py
+++ images.py
@@ -30,4 +30,5 @@ with open('IMAGES.md', 'w') as f_out:
             tags = image.get('tags', [])
             _tags = ', '.join([tag if tag.strip() != '' else '-' for tag in tags])
             _tag = tags[0]
-            f_out.write('[{package}]({github}) | {architecture} | [![](https://images.microbadger.com/badges/version/crossbario/{package}:{tag}.svg)](https://github.com/crossbario/crossbar-docker/blob/master/{package}/{architecture}/Dockerfile.{tag}) | [![](https://images.microbadger.com/badges/image/crossbario/{package}:{tag}.svg)](https://hub.docker.com/r/crossbario/{package}/) | `crossbario/{package}:{tag}`\n'.format(package=package, architecture=architecture, github=github, name=name, tag=_tag, tags=_tags))
+            for _tag in tags:
+                f_out.write('[{package}]({github}) | {architecture} | [![](https://images.microbadger.com/badges/version/crossbario/{package}:{tag}.svg)](https://github.com/crossbario/crossbar-docker/blob/master/{package}/{architecture}/Dockerfile.{tag}) | [![](https://images.microbadger.com/badges/image/crossbario/{package}:{tag}.svg)](https://hub.docker.com/r/crossbario/{package}/) | `crossbario/{package}:{tag}`\n'.format(package=package, architecture=architecture, github=github, name=name, tag=_tag, tags=_tags))
