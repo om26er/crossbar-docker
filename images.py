79f2be2783df7fdc0aea217a94b953233cede171
Tobias Oberstein <tobias.oberstein@crossbario.com>
2017-04-22 19:57:34 +0200

    wip

---
+++ images.py
@@ -0,0 +1,32 @@
+# [autobahn-js](https://github.com/crossbario/autobahn-js) | amd64 |
+
+#  | [![](https://images.microbadger.com/badges/image/crossbario/
+
+# [![](https://images.microbadger.com/badges/version/crossbario/autobahn-js:full.svg)](https://github.com/crossbario/crossbar-docker/blob/master/autobahn-js/x86_64/Dockerfile.full)
+
+def badger_url(package, flavor):
+    return '[![](https://images.microbadger.com/badges/version/crossbario/autobahn-js:full.svg)](https://github.com/crossbario/crossbar-docker/blob/master/autobahn-js/x86_64/Dockerfile.full)'
+
+import json
+from pprint import pprint
+
+HEADER = """
+# Images
+
+Package | Architecture | Flavor | Image | Image ID (`docker pull`)
+---|---|---|---|---
+"""
+
+with open('IMAGES.md', 'w') as f_out:
+    with open('images.json') as f_in:
+        data = f_in.read()
+        obj = json.loads(data)
+        f_out.write(HEADER)
+        for image in obj.get('images', []):
+            package = image.get('package', None)
+            architecture = image.get('architecture', None)
+            github = image.get('github', None)
+            name = image.get('name', None)
+            tags = image.get('tags', [])
+            for tag in tags:
+                f_out.write('[{package}]({github}) | {architecture} | [![](https://images.microbadger.com/badges/version/crossbario/{package}:{tag}.svg)](https://github.com/crossbario/crossbar-docker/blob/master/{package}/{architecture}/Dockerfile.full) | [![](https://images.microbadger.com/badges/image/crossbario/{package}:{tag}.svg)](https://hub.docker.com/r/crossbario/{package}/) | `crossbario/{package}:{tag}`\n'.format(package=package, architecture=architecture, github=github, name=name, tag=tag))
