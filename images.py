7d1cd9bf3869211d6db8fd89f39e7e166e085ffe
Tobias Oberstein <tobias.oberstein@crossbario.com>
2017-04-22 21:51:12 +0200

    wip

--- images.py
+++ images.py
@@ -33,5 +33,5 @@ with open('IMAGES.md', 'w') as f_out:
                 _tag = tags[0]
                 for _tag in tags:
                     image_id = 'crossbario/{package}{arch}:{tag}'.format(package=package, tag=_tag, arch='-{}'.format(architecture) if architecture else '')
-                    _tag = ':'.format(_tag) if _tag != 'x86_64' else ''
-                    f_out.write('[{package}]({github}) | {architecture} | [![](https://images.microbadger.com/badges/version/crossbario/{package}{tag}.svg)](https://github.com/crossbario/crossbar-docker/blob/master/{package}/{architecture}/Dockerfile.{tag}) | [![](https://images.microbadger.com/badges/image/crossbario/{package}:{tag}.svg)](https://hub.docker.com/r/crossbario/{package}/) | `{image_id}`\n'.format(package=package, architecture=architecture, github=github, name=name, tag=_tag, tags=_tags, image_id=image_id))
+                    _architecture = '-'.format(architecture) if architecture != 'x86_64' else ''
+                    f_out.write('[{package}]({github}) | {architecture} | [![](https://images.microbadger.com/badges/version/crossbario/{package}{tag}.svg)](https://github.com/crossbario/crossbar-docker/blob/master/{package}/{architecture}/Dockerfile.{tag}) | [![](https://images.microbadger.com/badges/image/crossbario/{package}:{tag}.svg)](https://hub.docker.com/r/crossbario/{package}/) | `{image_id}`\n'.format(package=package, architecture=_architecture, github=github, name=name, tag=_tag, tags=_tags, image_id=image_id))
