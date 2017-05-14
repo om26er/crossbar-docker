a822cee23045beb37682b66016d80935cb8b992a
Tobias Oberstein <tobias.oberstein@crossbario.com>
2017-05-14 16:41:45 +0200

    add testsuite image

--- versions.sh
+++ versions.sh
@@ -3,10 +3,11 @@
 #
 # CHANGE FOR NEW RELEASES (these need to be proper Git tags in the respective repo):
 #
-export CROSSBAR_VERSION='v17.5.1'
-export AUTOBAHN_JS_VERSION='v17.5.1'
-export AUTOBAHN_PYTHON_VERSION='v17.5.1'
-export AUTOBAHN_CPP_VERSION='v17.5.1'
+export CROSSBAR_VERSION='17.5.1'
+export AUTOBAHN_JS_VERSION='17.5.1'
+export AUTOBAHN_PYTHON_VERSION='17.5.1'
+export AUTOBAHN_CPP_VERSION='17.5.1'
+export AUTOBAHN_TESTSUITE_VERSION='0.7.6'
 #
 # END OF CONFIG
 #
@@ -15,10 +16,11 @@ export AUTOBAHN_CPP_VERSION='v17.5.1'
 # Git working directories of all relevant repos must reside
 # in parallel (as siblings) to this repository
 #
-export CROSSBAR_VCS_REF=`git --git-dir="../crossbar/.git" rev-list -n 1 ${CROSSBAR_VERSION} --abbrev-commit`
-export AUTOBAHN_JS_VCS_REF=`git --git-dir="../autobahn-js/.git" rev-list -n 1 ${AUTOBAHN_JS_VERSION} --abbrev-commit`
-export AUTOBAHN_PYTHON_VCS_REF=`git --git-dir="../autobahn-python/.git" rev-list -n 1 ${AUTOBAHN_PYTHON_VERSION} --abbrev-commit`
-export AUTOBAHN_CPP_VCS_REF=`git --git-dir="../autobahn-cpp/.git" rev-list -n 1 ${AUTOBAHN_CPP_VERSION} --abbrev-commit`
+export CROSSBAR_VCS_REF=`git --git-dir="../crossbar/.git" rev-list -n 1 v${CROSSBAR_VERSION} --abbrev-commit`
+export AUTOBAHN_JS_VCS_REF=`git --git-dir="../autobahn-js/.git" rev-list -n 1 v${AUTOBAHN_JS_VERSION} --abbrev-commit`
+export AUTOBAHN_PYTHON_VCS_REF=`git --git-dir="../autobahn-python/.git" rev-list -n 1 v${AUTOBAHN_PYTHON_VERSION} --abbrev-commit`
+export AUTOBAHN_CPP_VCS_REF=`git --git-dir="../autobahn-cpp/.git" rev-list -n 1 v${AUTOBAHN_CPP_VERSION} --abbrev-commit`
+export AUTOBAHN_TESTSUITE_VCS_REF=`git --git-dir="../autobahn-testsuite/.git" rev-list -n 1 v${AUTOBAHN_TESTSUITE_VERSION} --abbrev-commit`
 export BUILD_DATE=`date -u +"%Y-%m-%d"`
 
 echo ""
@@ -28,4 +30,5 @@ echo "crossbar ${CROSSBAR_VERSION} [${CROSSBAR_VCS_REF}]"
 echo "autobahn-js ${AUTOBAHN_JS_VERSION} [${AUTOBAHN_JS_VCS_REF}]"
 echo "autobahn-python ${AUTOBAHN_PYTHON_VERSION} [${AUTOBAHN_PYTHON_VCS_REF}]"
 echo "autobahn-cpp ${AUTOBAHN_CPP_VERSION} [${AUTOBAHN_CPP_VCS_REF}]"
+echo "autobahn-testsuite ${AUTOBAHN_TESTSUITE_VERSION} [${AUTOBAHN_TESTSUITE_VCS_REF}]"
 echo ""
